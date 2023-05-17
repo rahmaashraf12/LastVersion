86e4a78ee246b96692df5b92a07b93dcd65fd20c
hassan010802002 <92964808+hassan010802002@users.noreply.github.com>
2023-05-17 15:09:21 +0300

    Application New Version

---
+++ app/src/main/java/com/example/foodtime/Activity/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.foodtime.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.foodtime.Adapter.CategoryAdapter;
+import com.example.foodtime.Adapter.RecommendedAdapter;
+import com.example.foodtime.Domain.CategoryDomain;
+import com.example.foodtime.Domain.FoodDomain;
+import com.example.foodtime.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter adapter,adapter2;
+    private RecyclerView recyclerViewcategorylist,recyclerViewpopularlist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        racyclerviewcategory();
+        recyclerViewpopular();
+        bottomNavigation();
+    }
+
+    private void bottomNavigation() {
+        LinearLayout homeBtn =findViewById(R.id.homeBtn_Card);
+        LinearLayout cartBtn = findViewById(R.id.CartBtn);
+
+        homeBtn.setOnClickListener((v -> {
+            startActivity(new Intent(MainActivity.this,MainActivity.class));
+        }));
+
+        cartBtn.setOnClickListener((v -> {
+            startActivity(new Intent(MainActivity.this,CartActivity.class));
+        }));
+    }
+
+    private void recyclerViewpopular() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewpopularlist = findViewById(R.id.view2);
+        recyclerViewpopularlist.setLayoutManager(linearLayoutManager);
+
+        ArrayList<FoodDomain> foodlist = new ArrayList<>();
+        foodlist.add(new FoodDomain("Pepperoni Pizza","pizza1",
+                "slices pepperoni , mozzeralla cheese, fresh oregano, ground black pepper, pizza sauce",
+                13.0,5,20,1000));
+        foodlist.add(new FoodDomain("Cheese Burger","burger",
+                "Beef, Gouda Cheese , Special Sauce , Lettuce , tomato",
+                15.20,4,18,1500));
+        foodlist.add(new FoodDomain("Vegetable Pizza","pizza3",
+                "Olive Oil , Vegetable Oil , Pitted Kalamata , Cherry Tomatoes , Fresh Oregano , Basil",
+                11.0,3,16,800));
+
+        adapter2 = new RecommendedAdapter(foodlist);
+        recyclerViewpopularlist.setAdapter(adapter2);
+    }
+
+    private void racyclerviewcategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewcategorylist = findViewById(R.id.view1);
+        recyclerViewcategorylist.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> categoryList = new ArrayList<>();
+        categoryList.add(new CategoryDomain("Pizza","cat_1"));
+        categoryList.add(new CategoryDomain("Burger","cat_2"));
+        categoryList.add(new CategoryDomain("Hotdog","cat_3"));
+        categoryList.add(new CategoryDomain("Drink","cat_4"));
+        categoryList.add(new CategoryDomain("Donut","cat_5"));
+
+        adapter = new CategoryAdapter(categoryList);
+        recyclerViewcategorylist.setAdapter(adapter);
+    }
+}
